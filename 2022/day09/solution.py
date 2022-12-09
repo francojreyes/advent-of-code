10b8a8194c57182e832412722417098a878b5099
Franco Reyes <franco.javier.reyes@gmail.com>
2022-12-10 01:48:38 +1100

    Cleanup

--- 2022/day09/solution.py
+++ 2022/day09/solution.py
@@ -1,5 +1,6 @@
 from sys import stdin
 
+
 def translate(position: tuple[int, int], direction: str):
     match direction:
         case "U":
@@ -13,34 +14,38 @@ def translate(position: tuple[int, int], direction: str):
 
 
 def follow(tail: tuple[int, int], head: tuple[int, int]):
-    new_tail = tail
     diff = (head[0] - tail[0], head[1] - tail[1])
-
     if max(abs(diff[0]), abs(diff[1])) <= 1:
         return tail
 
-    if diff[0] > 0:
-        new_tail = translate(new_tail, "D")
-    elif diff[0] < 0:
+    new_tail = tail
+    if diff[0] < 0:
         new_tail = translate(new_tail, "U")
-    
-    if diff[1] > 0:
-        new_tail = translate(new_tail, "R")
-    elif diff[1] < 0:
+    elif diff[0] > 0:
+        new_tail = translate(new_tail, "D")
+    if diff[1] < 0:
         new_tail = translate(new_tail, "L")
+    elif diff[1] > 0:
+        new_tail = translate(new_tail, "R")
 
     return new_tail
 
 
-if __name__ == '__main__':
-    rope = [(0, 0)] * 10
+def solve(motions: list[str], num_knots: int) -> int:
+    rope = [(0, 0)] * num_knots
     visited = set()
     visited.add(rope[len(rope) - 1])
-    for line in stdin:
-        direction = line.split()[0]
-        for _ in range(int(line.split()[1])):
+    for motion in motions:
+        direction = motion.split()[0]
+        for _ in range(int(motion.split()[1])):
             rope[0] = translate(rope[0], direction)
             for i in range(1, len(rope)):
-                rope[i] = follow(rope[i], rope[i - 1]) 
+                rope[i] = follow(rope[i], rope[i - 1])
             visited.add(rope[len(rope) - 1])
-    print(len(visited))
+    return len(visited)
+
+
+if __name__ == '__main__':
+    motions = list(stdin)
+    print("Part 1:", solve(motions, 2))
+    print("Part 2:", solve(motions, 10))
