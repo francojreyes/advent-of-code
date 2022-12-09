612d103f7ec0d3b22506ee21b78aa39ab7e12412
Franco Reyes <franco.javier.reyes@gmail.com>
2022-12-09 17:57:12 +1100

    Remove scuff

--- 2022/day09/solution.py
+++ 2022/day09/solution.py
@@ -13,60 +13,23 @@ def translate(position: tuple[int, int], direction: str):
 
 
 def follow(tail: tuple[int, int], head: tuple[int, int]):
+    new_tail = tail
     diff = (head[0] - tail[0], head[1] - tail[1])
+
     if max(abs(diff[0]), abs(diff[1])) <= 1:
         return tail
-    match diff:
-        case (-2, 0):
-            return translate(tail, "U")
-        case (2, 0):
-            return translate(tail, "D")
-        case (0, -2):
-            return translate(tail, "L")
-        case (0, 2):
-            return translate(tail, "R")
-        case (1, 2):
-            return translate(translate(tail, "D"), "R")
-        case (-1, 2):
-            return translate(translate(tail, "U"), "R")
-        case (-2, -1):
-            return translate(translate(tail, "U"), "L")
-        case (-2, 1):
-            return translate(translate(tail, "U"), "R")
-        case (1, -2):
-            return translate(translate(tail, "D"), "L")
-        case (-1, -2):
-            return translate(translate(tail, "U"), "L")
-        case (2, -1):
-            return translate(translate(tail, "D"), "L")
-        case (2, 1):
-            return translate(translate(tail, "D"), "R")
-        case (-2, 2):
-            return translate(translate(tail, "U"), "R")
-        case (-2, -2):
-            return translate(translate(tail, "U"), "L")
-        case (2, -2):
-            return translate(translate(tail, "D"), "L")
-        case (2, 2):
-            return translate(translate(tail, "D"), "R")
-        case _:
-            raise ValueError(f"Invalid diff of {diff}")
 
+    if diff[0] > 0:
+        new_tail = translate(new_tail, "D")
+    elif diff[0] < 0:
+        new_tail = translate(new_tail, "U")
+    
+    if diff[1] > 0:
+        new_tail = translate(new_tail, "R")
+    elif diff[1] < 0:
+        new_tail = translate(new_tail, "L")
 
-def print_board(head, tail):
-    for i in range(-4, 1):
-        for j in range(6):
-            if head == (i, j):
-                print("H", end="")
-            elif tail == (i, j):
-                print("T", end="")
-            elif (i, j) == (0, 0):
-                print("s", end="") 
-            else:
-                print(".", end="")
-        print()
-    print()
-
+    return new_tail
 
 
 if __name__ == '__main__':
@@ -76,7 +39,6 @@ if __name__ == '__main__':
     for line in stdin:
         direction = line.split()[0]
         for _ in range(int(line.split()[1])):
-            # print_board(head, tail)
             rope[0] = translate(rope[0], direction)
             for i in range(1, len(rope)):
                 rope[i] = follow(rope[i], rope[i - 1]) 
