5c397e69cd266f9a4858d212a3c8a2b5fb02bcd1
Franco Reyes <franco.javier.reyes@gmail.com>
2025-12-02 00:56:08 -0500

    code nicer

--- 2025/day02/part1.ml
+++ 2025/day02/part1.ml
@@ -1,21 +1,25 @@
 open! Core
 
+let parse_range range_str =
+  let lo, hi =
+    String.lsplit2_exn ~on:'-' range_str |> Tuple2.map ~f:Int.of_string
+  in
+  Sequence.range ~start:`inclusive ~stop:`inclusive lo hi
+
+let is_invalid id =
+  let n_digits = Int.to_string id |> String.length in
+  match n_digits % 2 = 0 with
+  | false -> false
+  | true ->
+      let power = Int.(10 ** (n_digits / 2)) in
+      id % power = id / power
+
 let () =
   In_channel.input_line_exn In_channel.stdin
   |> String.split ~on:','
   |> Sequence.of_list
-  |> Sequence.concat_map ~f:(fun range_str ->
-    let lo, hi =
-      String.lsplit2_exn ~on:'-' range_str |> Tuple2.map ~f:Int.of_string
-    in
-    Sequence.range ~start:`inclusive ~stop:`inclusive lo hi)
-  |> Sequence.filter ~f:(fun x ->
-    let n_digits = Int.to_string x |> String.length in
-    match n_digits % 2 = 0 with
-    | false -> false
-    | true ->
-      let power = Int.(10 ** (n_digits / 2)) in
-      x % power = x / power)
+  |> Sequence.concat_map ~f:parse_range
+  |> Sequence.filter ~f:is_invalid
   |> Sequence.sum (module Int) ~f:Fn.id
   |> Int.to_string
   |> print_endline
--- 2025/day02/part2.ml
+++ 2025/day02/part2.ml
@@ -1,22 +1,26 @@
 open! Core
 
+let parse_range range_str =
+  let lo, hi =
+    String.lsplit2_exn ~on:'-' range_str |> Tuple2.map ~f:Int.of_string
+  in
+  Sequence.range ~start:`inclusive ~stop:`inclusive lo hi
+
+let is_invalid id =
+  let id = Int.to_string id in
+  let chars = String.to_list id in
+  Sequence.range ~start:`inclusive ~stop:`inclusive 1 (String.length id / 2)
+  |> Sequence.exists ~f:(fun chunk_length ->
+    List.chunks_of ~length:chunk_length chars
+    |> List.all_equal ~equal:[%equal: char list]
+    |> Option.is_some)
+
 let () =
   In_channel.input_line_exn In_channel.stdin
   |> String.split ~on:','
   |> Sequence.of_list
-  |> Sequence.concat_map ~f:(fun range_str ->
-    let lo, hi =
-      String.lsplit2_exn ~on:'-' range_str |> Tuple2.map ~f:Int.of_string
-    in
-    Sequence.range ~start:`inclusive ~stop:`inclusive lo hi)
-  |> Sequence.filter ~f:(fun x ->
-    let x = Int.to_string x in
-    let chars = String.to_list x in
-    Sequence.range ~start:`inclusive ~stop:`inclusive 1 (String.length x / 2)
-    |> Sequence.exists ~f:(fun chunk_length ->
-      List.chunks_of ~length:chunk_length chars
-      |> List.all_equal ~equal:[%equal: char list]
-      |> Option.is_some))
+  |> Sequence.concat_map ~f:parse_range
+  |> Sequence.filter ~f:is_invalid
   |> Sequence.sum (module Int) ~f:Fn.id
   |> Int.to_string
   |> print_endline
